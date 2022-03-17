d94595c4dd416367b78346c7fa6dea72a37f6c04
Payal Jindal <payal.jindal.19.1@mountblue.tech>
2022-03-17 17:43:51 +0530

    LimitFunction modified

--- test/testLimitFunctionCallCount.js
+++ test/testLimitFunctionCallCount.js
@@ -1,6 +1,6 @@
 let limitFunction = require('../limitFunctionCallCount.js');
 
-limitFunctionObject = limitFunction(cb = () =>{ return "Call back funtion called"}, 5);
+let limitFunctionObject = limitFunction(cb = () =>{ return "Call back funtion called"}, 5);
 console.log(limitFunctionObject());
 console.log(limitFunctionObject());
 console.log(limitFunctionObject());
